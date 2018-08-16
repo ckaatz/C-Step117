deef45e4a633c99a4f800c972fbe9d2a8cda5879
ckaatz <39869540+ckaatz@users.noreply.github.com>
2018-08-15 21:27:23 -0500

    Add project files.

---
+++ C-Step117/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Step117
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MoreMath Math1 = new MoreMath();
+            Console.WriteLine("Pick a number:");
+            int UserInput = Convert.ToInt32(Console.ReadLine());
+            int OutPutValue;
+            Math1.DivideByTwo(UserInput, out OutPutValue);
+            Console.WriteLine(OutPutValue);
+            Console.ReadLine();
+        }
+    }
+
+    public class MoreMath
+    {
+        public void DivideByTwo(int Num1, out int Num2)
+        {
+            Num2 = Num1 / 2;
+        }
+    }
+}
